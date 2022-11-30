8de259cfa96224f973905e3789ebc8488c41bce9
Ершова Ольга <ershova@superevm.ru>
2022-11-30 13:18:30 +0300

    solution for part 2.2 step 6 of the course added

---
+++ part_22-2.py
@@ -0,0 +1,38 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(by='id', value='input_value')
+    x = x_element.text
+    y = calc(x)
+    
+    input1 = browser.find_element(by='id', value='answer')
+    input1.send_keys(y)
+
+    browser.execute_script("window.scrollBy(0, 100);")
+
+    option1 = browser.find_element(by='id', value='robotCheckbox')
+    option1.click()
+    option2 = browser.find_element(by='id', value='robotsRule')
+    option2.click()
+
+    button = browser.find_element(by='css selector', value="button.btn")
+    button.click()
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
